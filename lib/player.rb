77a4a3b5c1b9a26190ee9fbd18448736b9dff5fd
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-24 21:15:22 +0000

    Automatically backed up by Learn

--- lib/player.rb
+++ lib/player.rb
@@ -1,9 +1,9 @@
-class Player
+require 'pry'
 
+class Player
   attr_reader :token
 
   def initialize(token)
     @token = token
   end
-
 end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,11 +1,11 @@
+require 'pry'
+
 module Players
   class Human < Player
 
-    def move(board)
-      puts "Please enter 1-9:"
-      gets.strip
+    def move(input)
+      input = gets.chomp
     end
 
   end
-
 end
