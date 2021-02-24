cbfaae8732b8dac0856fe81bb2709a16988b34d7
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-24 21:20:44 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,7 @@ module Players
   class Computer < Player
 
     def move(board)
-      valid_move = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+      valid_move = [1, 2, 3, 4, 5, 6, 7, 8, 9]
       !board.taken?(valid_move)
      binding.pry
     end
